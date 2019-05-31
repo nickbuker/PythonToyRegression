153d5bd66d33bd0a323fc908624733d765ab8b95
Buker, Nick <Nick.Buker@nordstrom.com>
2019-05-31 08:25:20 -0700

    update score func names

--- test/test_scoring.py
+++ test/test_scoring.py
@@ -7,21 +7,21 @@ from ..src import scoring
 def test_rss():
     y = np.ones(10)
     y_hat = np.arange(1, 11)
-    assert scoring.RSS(y, y_hat) == 285.0
+    assert scoring.rss(y, y_hat) == 285.0
 
 
 def test_tss():
     y = np.arange(1, 11)
-    assert scoring.TSS(y) == 82.5
+    assert scoring.tss(y) == 82.5
 
 
 def test_ess():
     y = np.ones(10)
     y_hat = np.arange(1, 11)
-    assert scoring.ESS(y, y_hat) == 285.0
+    assert scoring.ess(y, y_hat) == 285.0
 
 
 def test_r2():
     y = np.arange(1, 11)
     y_hat = np.arange(1, 11)
-    assert scoring.R2(y, y_hat) == 1.0
+    assert scoring.r2(y, y_hat) == 1.0
